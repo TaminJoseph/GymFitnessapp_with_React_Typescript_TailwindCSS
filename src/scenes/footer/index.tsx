6624a3b1e6571407ce94f5a19a48ad386acc4a26
idrismuhd <idrismuhd41@gmail.com>
2023-06-10 12:19:40 +0100

    first commit

---
+++ src/scenes/footer/index.tsx
@@ -0,0 +1,30 @@
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem blanditiis molestias deserunt alias facere eaque cum nobis dolorem ipsa minus.
+          </p>
+          <p>© Sunusi All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+          <p className="my-5">Unity Estae Jaba</p>
+          <p className="my-5">Naassarawa GRA Area</p>
+          <p>Kano Nigeria</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
+          <p>08088682089</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
